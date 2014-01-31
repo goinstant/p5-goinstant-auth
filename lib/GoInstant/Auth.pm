f5d26f484f7387eada1474395774e85fa157dc3f
Jeremy Stashewsky <jeremy@goinstant.com>
2014-01-31 13:24:45 -0800

    implementation

--- lib/GoInstant/Auth.pm
+++ lib/GoInstant/Auth.pm
@@ -5,7 +5,13 @@ use warnings;
 
 our $VERSION = "0.01";
 
+use GoInstant::Auth::Signer;
 
+sub Signer($) {
+    return GoInstant::Auth::Signer->new({
+        key => $_[0]
+    });
+}
 
 1;
 __END__
@@ -14,26 +20,65 @@ __END__
 
 =head1 NAME
 
-GoInstant::Auth - It's new $module
+GoInstant::Auth - GoInstant Authentication for Your Perl Application
 
 =head1 SYNOPSIS
 
     use GoInstant::Auth;
 
+    my $signer = GoInstant::Auth->Signer($key);
+
+    var $jwt = $signer->sign({
+        user_id => 'permanentid',
+        display_name => 'Bob Smith',
+        groups => [
+            { id => 'bobgroup', display_name => "Bob's Group" }
+        ]
+    });
+
 =head1 DESCRIPTION
 
-GoInstant::Auth is ...
+This is an implementation of JWT tokens consistent with what's specified in the
+L<GoInstant Users and Authentication Guide|https://developers.goinstant.com/v1/guides/users_and_authentication.html>.
+
+This library is not intended as a general-use JWT library.  In fact, this
+module is a thin wrapper around L<JSON::WebToken>, which should be sufficient
+for more advanced use-cases.
+
+=head1 METHODS
+
+=head2 Signer($key)
+
+Returns a L<GoInstant::Auth::Signer> object, which can be used to create and sign many JWTs.  Please see L<GoInstant::Auth::Signer/sign> for details.
+
+=head1 CONTRIBUTING
+
+This module uses the L<Minilla> CPAN module authoring tool.
+
+=head2 Development Pre-requisites
+
+=over 4
+
+=item perl >= 5.8.1
+
+=item L<Minilla> >= 0.11.0
+
+=back
+
+=head2 Running Tests
+
+Simply run C<minil test>.  Tests are located in the C<t/> directory.
+
+=head1 SEE ALSO
+
+L<JSON::WebToken> for generic JWT operations.
 
 =head1 LICENSE
 
-Copyright (C) Jeremy Stashewsky.
+Copyright (C) 2014 GoInstant Inc., a salesforce.com company.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+Licensed under the 3-Clause BSD Licence (see LICENCE.txt).
 
 =head1 AUTHOR
 
-Jeremy Stashewsky E<lt>jeremy@goinstant.comE<gt>
-
-=cut
-
+Jeremy Stashewsky E<lt>jeremy@goinstant.comE<gt> for GoInstant Inc. E<lt>support@goinstant.comE<gt>
---
+++ lib/GoInstant/Auth/Signer.pm
@@ -0,0 +1,174 @@
+package GoInstant::Auth::Signer;
+use 5.008005;
+use strict;
+use warnings;
+
+use MIME::Base64 qw(decode_base64);
+
+sub _decode_key {
+    my $key = shift;
+    $key =~ tr{_-}{/+};
+    $key .= '=' while length($key) % 4;
+    return decode_base64($key);
+}
+
+sub new {
+    my $clazz = shift;
+    my $self = bless {}, $clazz;
+    my %args = @_;
+
+    $self->{key} = _decode_key($args{key});
+    $self->{alg} = $args{alg} || 'HS256';
+
+    if ($self->{alg} != 'HS256' &&
+        $self->{alg} != 'HS384' &&
+        $self->{alg} != 'HS512'
+    ) {
+        die 'Invalid signature algorithm; '.
+            'GoInstant supports HS256/HS384/HS512';
+    }
+
+    return $self;
+}
+
+my %REQUIRED = (
+    domain => 'iss',
+    user_id => 'sub',
+    display_name => 'dn',
+);
+
+my %OPTIONAL = (
+    email => 'email',
+    avatar_url => 'avatarUrl'
+);
+
+my %GROUP_REQUIRED = (
+    id => 'id',
+    display_name => 'dn',
+);
+
+sub sign {
+    my $self = shift;
+    my $args = shift;
+
+    my $k;
+    my %claims = ();
+
+    for $k (keys %REQUIRED) {
+        die "$k is required" unless defined $args->{$k};
+        $claims{$REQUIRED{$k}} = $args->{$k};
+    }
+
+    for $k (keys %OPTIONAL) {
+        $claims{$OPTIONAL{$k}} = $args->{$k}
+            if defined $args->{$k};
+    }
+
+    if ($args->{groups}) {
+        $claims{g} = [];
+        for my $group (@{$args->{groups}}) {
+            my $g_claims = {};
+            for $k (keys %GROUP_REQUIRED) {
+                die "group $k is required" unless defined $group->{$k};
+                $g_claims->{$GROUP_REQUIRED{$k}} = $group->{$k};
+            }
+            push @{$claims{g}}, $g_claims;
+        }
+    }
+
+    return JSON::WebToken->encode(\%claims, $self->{key}, $self->{alg});
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+GoInstant::Auth::Signer
+
+=head1 SYNOPSIS
+
+    use GoInstant::Auth::Signer;
+
+    my $signer = GoInstant::Auth::Signer->new({
+        key => $secret_key_in_base64
+    });
+
+    my $user = {
+        domain => 'example.com',
+        user_id => 42,
+        display_name => 'Bob Smith',
+        groups => [
+            { id => 'bobgroup', display_name => "Bob's Group" }
+        ]
+    };
+    my $jwt = $signer->sign($user);
+    print "jwt to pass to GoInstant is",$jwt,$/;
+
+=head1 METHODS
+
+=head2 new($key)
+
+Creates a new Signer object.  C<$key> must be a string with a Base64 or
+Base64url encoded key.
+
+=head2 sign($user)
+
+Creates a signed JWT for the supplied user.  Currently, an HMAC-SHA256
+signature (the C<HS256> algorithm) is used.
+
+=head3 User Information
+
+The C<sign()> method must be passed a hash of user information.  The fields of
+that hash are as follows:
+
+=over 4
+
+=item domain
+
+The domain of this user, in which the C<user_id> is unique.
+
+=item user_id
+
+The permanent identifier of this user on your system, unique at least within
+your C<domain>.
+
+Database row-IDs might be a good choice, but email addresses (which can in
+theory change) are not.
+
+=item display_name
+
+The name to display for this user.
+
+=item groups
+
+I<Optional> An array of groups that this user belongs to, which can be used within
+GoInstant to enforce access controls.
+
+=over 4
+
+=item id
+
+The permanent, unique id of this group within the above C<domain>.
+
+=item display_name
+
+I<Optional> The name to display for this group.
+
+=back
+
+=item email
+
+I<Optional> Email address to add to the JWT.
+
+=item avatar_url
+
+I<Optional> URL to a small picture to display for this user.
+
+=back
+
+=head1 SEE ALSO
+
+L<JSON::WebToken> for generic JWT operations.
