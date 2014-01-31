1f2bd0dd1a533ed8e56e1864ddfbc0e792b54d66
Jeremy Stashewsky <jeremy@goinstant.com>
2014-01-31 12:32:47 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+GoInstant::Auth - It's new $module
+
+# SYNOPSIS
+
+    use GoInstant::Auth;
+
+# DESCRIPTION
+
+GoInstant::Auth is ...
+
+# LICENSE
+
+Copyright (C) Jeremy Stashewsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jeremy Stashewsky <jeremy@goinstant.com>
---
+++ lib/GoInstant/Auth.pm
@@ -0,0 +1,39 @@
+package GoInstant::Auth;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+GoInstant::Auth - It's new $module
+
+=head1 SYNOPSIS
+
+    use GoInstant::Auth;
+
+=head1 DESCRIPTION
+
+GoInstant::Auth is ...
+
+=head1 LICENSE
+
+Copyright (C) Jeremy Stashewsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jeremy Stashewsky E<lt>jeremy@goinstant.comE<gt>
+
+=cut
+
